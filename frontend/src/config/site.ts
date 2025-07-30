adc3572f1f5fd476c68146ed64b6c8322a649a1a
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-07-30 18:04:40 +0300

    feat: update site configuration with new branding and enhanced description for zeroinch

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -1,9 +1,10 @@
 import { env } from "@/env.mjs"
 
 export const siteConfig = {
-  name: "ZeroInch",
-  author: "ZeroInch",
-  description: "Trade using 1inch's limit order protocol privately",
+  name: "zeroinch",
+  author: "zeroinch",
+  description:
+    "Trade using 1inch's limit order protocol privately using zeroinch!",
   keywords: ["1inch", "limit order", "private", "trade"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
