ead6407ca085a09c8deec077667f7f92694394d8
yoyoismee <yoyoismee@gmail.com>
2025-07-30 16:48:12 +0300

    update circuit and test

--- contract/src/Zeroinch.sol
+++ contract/src/Zeroinch.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 import {IVerifier} from "./Verifier.sol";
 import "@1inch/solidity-utils/contracts/mixins/OnlyWethReceiver.sol";
 import {IPreInteraction} from "limit-order-protocol/interfaces/IPreInteraction.sol";
+import {IPostInteraction} from "limit-order-protocol/interfaces/IPostInteraction.sol";
 import {IOrderMixin} from "limit-order-protocol/interfaces/IOrderMixin.sol";
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
@@ -12,7 +13,19 @@ import "./PoseidonT4.sol";
 
 import "./MerkleTreeWithHistory.sol";
 
-contract Zeroinch is IPreInteraction, MerkleTreeWithHistory(20) {
+contract Zeroinch is
+    IPreInteraction,
+    IPostInteraction,
+    MerkleTreeWithHistory(20)
+{
+    address private immutable _LIMIT_ORDER_PROTOCOL;
+
+    modifier onlyLimitOrderProtocol() {
+        if (msg.sender != _LIMIT_ORDER_PROTOCOL)
+            revert OnlyLimitOrderProtocol();
+        _;
+    }
+    error OnlyLimitOrderProtocol();
     IVerifier _verifier;
     enum OrderType {
         SimpleLimit,
@@ -34,8 +47,9 @@ contract Zeroinch is IPreInteraction, MerkleTreeWithHistory(20) {
     mapping(bytes32 => bool) public nullifierHashes;
     mapping(bytes32 => bool) public insertedNotes;
 
-    constructor(address _verifierAddress) {
+    constructor(address _verifierAddress, address limitOrderProtocol) {
         _verifier = IVerifier(_verifierAddress);
+        _LIMIT_ORDER_PROTOCOL = limitOrderProtocol;
     }
 
     function _verify(
@@ -68,9 +82,29 @@ contract Zeroinch is IPreInteraction, MerkleTreeWithHistory(20) {
     }
 
     function order() public {}
-    function withdraw() public {}
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to,
+        uint256 nonce
+    ) public {
+        bytes32 orderHash = keccak256(
+            abi.encodePacked(asset, amount, to, nonce)
+        );
+    }
 
     function preInteraction(
+        IOrderMixin.Order calldata order,
+        bytes calldata extension,
+        bytes32 orderHash,
+        address taker,
+        uint256 makingAmount,
+        uint256 takingAmount,
+        uint256 remainingMakingAmount,
+        bytes calldata extraData
+    ) external {}
+
+    function postInteraction(
         IOrderMixin.Order calldata order,
         bytes calldata extension,
         bytes32 orderHash,
